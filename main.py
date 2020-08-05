036ad6d0cf8e32e3c47984425ab40db170ba56bd
calebkim107 <calebkim1117@gmail.com>
2020-08-05 21:11:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+
+# for i in range(1, 7):
+#  for j in range(1, i):
+#    print(j, end = "")
+#  print()
+
+# for i in range(5):
+#  print(i)
+
+
+# for i in range(5):
+#  for j in range(4):
+#    print("hello")
+
+
+num = input("Enter number: ")
+num = float(num)
+
+
+# condition to assess whether num is positive, negative, or zero
+
+if(num > 0):
+  print("positive")
+elif(num < 0):
+  print("negative")
+else:
+  print("that's neither man")
+
